e2d86a2373077cda7a3cd45966fbb7a5cdd37d60
wnderbin <->
2025-04-28 23:52:20 +0900

    order model changes

--- order-service/models/order.go
+++ order-service/models/order.go
@@ -1,1 +1,9 @@
 package models
+
+type Order struct {
+	Id              string `json:"id"`
+	User_id         string `json:"user_id"`
+	Restaurant_id   string `json:"restaurant_id"`
+	Price           int    `json:"price"`
+	Delivery_status string `json:"status"`
+}
